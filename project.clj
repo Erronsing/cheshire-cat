2ac3dc6f62dc007c03e385dd8aa6926b44d69327
Erronsing <garthbjerk@gmail.com>
2017-05-09 17:18:05 +0200

    CLJS and HTML

--- project.clj
+++ project.clj
@@ -3,11 +3,20 @@
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.521"]
                  [compojure "1.5.1"]
                  [ring/ring-defaults "0.2.1"]
                  [ring/ring-json "0.4.0"]]
-  :plugins [[lein-ring "0.9.7"]]
+  :plugins [[lein-ring "0.9.7"]
+            [lein-cljsbuild "1.1.6"]]
   :ring {:handler cheshire-cat.handler/app}
+  :cljsbuild {
+              :builds [{
+                        :source-paths ["src-cljs"]
+                        :compiler {
+                                   :output-to "resources/public/main.js"
+                                   :optimizations :whitespace
+                                   :pretty-print true}}]}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring/ring-mock "0.3.0"]]}})
